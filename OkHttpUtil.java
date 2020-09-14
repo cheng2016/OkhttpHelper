256d00dd1c34dda9413a0315dc99fa2aa59b300d
mitnick <1102743539@qq.com>
2020-09-14 11:02:21 +0800

    优化逻辑，使用局部变量代替静态变量的loadingBar彻底解决可能导致内存泄漏的问题

--- OkHttpUtil.java
+++ OkHttpUtil.java
@@ -1,18 +1,24 @@
+
 import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
-
-import com.icloud.sdk.view.LoadingBar;
 import android.util.Log;
 
+import com.huyu.sdk.view.LoadingBar;
+
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -21,23 +27,26 @@ import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 
 /**
- * Created by admin on 2019/6/14.
+ * Created by chengzj on 2019/6/14.
  * <p>
  * 模仿Async-Http通信库写出的okhttp子线程跨主线程通信类
  * <p>
- * 参考AsyncHttpClient、AsyncHttpResponseHandler类
+ * 主要参考AsyncHttpClient、AsyncHttpResponseHandler类
  * 参考Retofit2中OkHttpCall类、okhttp3中RealCall类
+ * 主要采用了适配器模式、模板模式
  * <p>
  * loadingBar 加载框
- * com.squareup.okhttp3:okhttp:3.10.0
- * com.squareup.okio:okio:2.1.0
- * com.squareup.okhttp3:logging-interceptor:3.9.1
  */
-public class OkHttpUtil {
-    private static final String TAG = "OkHttpUtil";
-    private static LoadingBar loadingBar;
-    private static volatile boolean isSetLoading = false;
+
+public class OkHttpUtils {
+    private static final String TAG = "OkHttpUtils";
+
     private static OkHttpClient client;
+
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newCachedThreadPool();
+
+    private OkHttpUtils() {
+    }
 
     static {
         HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
@@ -49,86 +58,120 @@ public class OkHttpUtil {
                 .build();
     }
 
-    private OkHttpUtil() {
+    public static void postLoading(Context context, String url, Map<String, String> paramsMap, final SimpleResponseHandler responseHandler) {
+        Logger.d("postLoading", "url:" + url + " requestData: " + Arrays.asList(paramsMap).toString());
+        FormBody.Builder builder = new FormBody.Builder();
+        for (String key : paramsMap.keySet()) {
+            //追加表单信息
+            builder.add(key, paramsMap.get(key));
+        }
+        responseHandler.isSetLoading = true;
+        postForm(context, url, builder.build(), responseHandler);
     }
-    
-    public static void post(final Context ctx, String url, String jsonStr, final SimpleResponseHandler responseHandler) {
-        isSetLoading = true;
-        loadingBar = new LoadingBar(ctx);
-        Log.d("post", "url:" + url + "\njsonStr:" + jsonStr);
+
+    public static void postNoLoading(String url, Map<String, String> paramsMap, final SimpleResponseHandler responseHandler) {
+        Logger.d("postNoLoading", "url:" + url + " requestData: " + Arrays.asList(paramsMap).toString());
+        FormBody.Builder builder = new FormBody.Builder();
+        for (String key : paramsMap.keySet()) {
+            //追加表单信息
+            builder.add(key, paramsMap.get(key));
+        }
+        responseHandler.isSetLoading = false;
+        postForm(url, builder.build(), responseHandler);
+    }
+
+    public static void postForm(String url, FormBody formBody, final SimpleResponseHandler responseHandler) {
+        postForm(null, url, formBody, responseHandler);
+    }
+
+    public static void postForm(Context context, String url, FormBody formBody, final SimpleResponseHandler responseHandler) {
+        Request request = new Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build();
+        Call call = client.newCall(request);
+        responseHandler.context = context;
+        EXECUTOR_SERVICE.execute(new ResponseRunnable(call, responseHandler));
+    }
+
+    public static void postMarkdown(Context context, String url, String jsonStr, final SimpleResponseHandler responseHandler) {
+        Logger.d(TAG, "post url:" + url + "\njsonStr:" + jsonStr);
         MediaType mediaType = MediaType.parse("text/x-markdown; charset=utf-8");
         Request request = new Request.Builder()
                 .url(url)
                 .post(RequestBody.create(mediaType, jsonStr))
                 .build();
-//        client.newCall(request).enqueue(responseHandler);
         Call call = client.newCall(request);
-        getDefaultThreadPool().execute(new ResponseRunnable(call,responseHandler));
+        responseHandler.isSetLoading = true;
+        responseHandler.context = context;
+        EXECUTOR_SERVICE.execute(new ResponseRunnable(call, responseHandler));
     }
-    
-    public static void postNoLoading(Context ctx, String url, String jsonStr, final SimpleResponseHandler responseHandler) {
-        isSetLoading = false;
-        Log.d("post", "url:" + url + "\njsonStr:" + jsonStr);
-        MediaType mediaType = MediaType.parse("text/x-markdown; charset=utf-8");
+
+    public static void postJson(Context context, String url, JSONObject jsonStr, final SimpleResponseHandler responseHandler) {
+        Logger.d(TAG, "postNoLoading url:" + url + "\njsonStr:" + jsonStr);
+        MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
         Request request = new Request.Builder()
                 .url(url)
-                .post(RequestBody.create(mediaType, jsonStr))
+                .post(RequestBody.create(mediaType, jsonStr.toString()))
                 .build();
-//        client.newCall(request).enqueue(responseHandler);
         Call call = client.newCall(request);
-        getDefaultThreadPool().execute(new ResponseRunnable(call,responseHandler));
+        responseHandler.isSetLoading = true;
+        responseHandler.context = context;
+        EXECUTOR_SERVICE.execute(new ResponseRunnable(call, responseHandler));
     }
-    
+
     public static void get(String url, final SimpleResponseHandler responseHandler) {
         Request request = new Request.Builder()
                 .get()
                 .url(url)
                 .build();
-//         client.newCall(request).enqueue(responseHandler);
         Call call = client.newCall(request);
-        getDefaultThreadPool().execute(new ResponseRunnable(call,responseHandler));
+        EXECUTOR_SERVICE.execute(new ResponseRunnable(call, responseHandler));
     }
 
-    private static ExecutorService getDefaultThreadPool() {
-        return Executors.newCachedThreadPool();
+    public static void release() {
+
     }
-    
-    private static class ResponseRunnable implements Runnable{
+
+    private static class ResponseRunnable implements Runnable {
         private Call call;
         private SimpleResponseHandler callback;
 
-        public ResponseRunnable(Call call,SimpleResponseHandler callback) {
+        public ResponseRunnable(Call call, SimpleResponseHandler callback) {
             this.call = call;
             this.callback = callback;
-            callback.sendStartMessage();
         }
 
         @Override
         public void run() {
             try {
+                callback.sendStartMessage();
                 Response response = call.execute();
-                callback.onResponse(call,response);
+                callback.onResponse(call, response);
             } catch (IOException e) {
-                callback.onFailure(call,e);
-            }finally {
-                callback.sendFinishMessage();
+                e.printStackTrace();
+                callback.onFailure(call, e);
             }
+            callback.sendFinishMessage();
         }
     }
-    
+
     /**
      * 模板模式-----定义算法的步骤，并把这些实现延迟到子类
      */
     public abstract static class SimpleResponseHandler implements Callback {
         private Handler handler;
 
+        private Context context;
+        private boolean isSetLoading = false;
+        private LoadingBar loadingBar;
+
         public SimpleResponseHandler() {
             Looper looper = Looper.myLooper();
             this.handler = new ResultHandler(this, looper);
         }
-        
+
         public void handleMessage(Message message) {
-            Log.d(TAG, "SimpleResponseHandler   handleMessage current Thread: " + Thread.currentThread().getName() +", message.what() == " + message.what);
             switch (message.what) {
                 case -1:
                     onStart();
@@ -147,10 +190,10 @@ public class OkHttpUtil {
                     break;
             }
         }
-        
+
         @Override
         public void onResponse(Call call, Response response) throws IOException {
-            Log.i(TAG, "SimpleResponseHandler   onResponse current Thread: " + Thread.currentThread().getName() + " , ThreadId : " + Thread.currentThread().getId());
+            Logger.d(TAG, "SimpleResponseHandler   onResponse current Thread: " + Thread.currentThread().getName() + " , ThreadId : " + Thread.currentThread().getId());
             if (response.code() < 200 || response.code() >= 300) {
                 sendFailuerMessage(new IOException(response.message()));
             } else {
@@ -160,49 +203,58 @@ public class OkHttpUtil {
 
         @Override
         public void onFailure(Call call, IOException e) {
-            Log.i(TAG, "SimpleResponseHandler   onFailure current Thread: " + Thread.currentThread().getName() + " , ThreadId : " + Thread.currentThread().getId());
+            Log.e(TAG, "SimpleResponseHandler   onFailure current Thread: " + Thread.currentThread().getName() + " , ThreadId : " + Thread.currentThread().getId());
             sendFailuerMessage(e);
         }
 
         void sendStartMessage() {
-            this.handler.sendMessage(obtainMessage(-1,null));
+            this.handler.sendMessage(obtainMessage(-1, null));
         }
 
         void sendSuccessMessage(int code, Call call, Response response) {
-            this.handler.sendMessage(obtainMessage(0,new Object[]{call,response}));
+            this.handler.sendMessage(obtainMessage(0, new Object[]{call, response}));
         }
 
         void sendFailuerMessage(Throwable throwable) {
-            this.handler.sendMessage(obtainMessage(1,throwable));
+            this.handler.sendMessage(obtainMessage(1, throwable));
         }
 
         void sendFinishMessage() {
-            this.handler.sendMessage(obtainMessage(2,null));
+            this.handler.sendMessage(obtainMessage(2, null));
         }
-        
-        public Message obtainMessage(int responseMessageId, Object responseMessageData) {
+
+        Message obtainMessage(int responseMessageId, Object responseMessageData) {
             return Message.obtain(this.handler, responseMessageId, responseMessageData);
         }
-        
+
         public void onStart() {
-            Log.d(TAG, "SimpleResponseHandler    onStart");
-            if (isSetLoading) {
+            Logger.d(TAG, "SimpleResponseHandler    onStart");
+            if (isSetLoading && context != null) {
+                Logger.i(TAG, "SimpleResponseHandler    loadingBar show");
+                loadingBar = new LoadingBar(context);
                 loadingBar.show();
             }
+
         }
 
         public void onFinish() {
-            Log.d(TAG, "SimpleResponseHandler    onFinish");
-            if (isSetLoading || (loadingBar != null && loadingBar.isShowing())) {
-                loadingBar.cancel();
+            Logger.d(TAG, "SimpleResponseHandler    onFinish");
+            if (isSetLoading) {
+                Logger.i(TAG, "SimpleResponseHandler    loadingBar hide");
+                if (loadingBar != null)
+                    loadingBar.cancel();
+                loadingBar = null;
             }
         }
-        
+
         public abstract void onSuccess(Call call, Response response);
 
-        public abstract void onFailure(Exception e);
+        public abstract void onFailure(Exception error);
     }
 
+    /**
+     * 类适配器模式------将一个类的接口，转换为客户期盼的另一个接口。通过继承的方式.
+     */
     private static class ResultHandler extends Handler {
         SimpleResponseHandler responseHandler;
 
